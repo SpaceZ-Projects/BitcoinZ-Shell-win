0bd9c2453ccc3b0d2f3f28c4ed22794397bb944d
ezzygarmyz <dragkhai@gmail.com>
2024-08-25 14:55:19 +0200

    building script

---
+++ build-win.bat
@@ -0,0 +1,20 @@
+@echo off
+
+
+echo Creating a virtual environment
+python -m venv env
+
+echo Activating the virtual environment
+call venv\Scripts\activate.bat
+
+echo Installing PyInstaller
+pip install PyInstaller
+
+echo Building excutable file...
+pyinstaller main.spec
+
+echo Deactivating the virtual environment
+deactivate
+
+echo Done!
+pause
