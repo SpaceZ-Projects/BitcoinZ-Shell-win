7c9d8388ed235dc0be6dc15f2be4b1a52ceabb5f
ezzygarmyz <dragkhai@gmail.com>
2024-09-01 16:35:26 +0200

    update build-win.bat

--- build-win.bat
+++ build-win.bat
@@ -1,5 +1,12 @@
 @echo off
+setlocal
 
+where python >nul 2>nul
+if %ERRORLEVEL% neq 0 (
+    echo Python is not installed. Please install Python and try again.
+    pause
+    exit /b 1
+)
 
 echo Creating a virtual environment
 python -m venv env
@@ -14,7 +21,16 @@ echo Building excutable file...
 pyinstaller main.spec
 
 echo Deactivating the virtual environment
-deactivate
+call env\Scripts\deactivate.bat
+
+echo Cleaning...
+powershell -Command "Remove-Item -Recurse -Force .\env"
+powershell -Command "Remove-Item -Recurse -Force .\build"
+
+move .\dist\*.exe .\
+
+powershell -Command "Remove-Item -Recurse -Force .\dist"
 
 echo Done!
+endlocal
 pause
